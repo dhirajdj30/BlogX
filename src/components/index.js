124756de120ac614bcf7ab0f24aed14ae9371db2
dhiraj Jagtap <dhirajdj30@gmail.com>
2024-07-10 03:11:54 +0530

    Updated on 10-7-24

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostCard from "./PostCard"
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout"
+import Button from "./Button";
+import Select from "./Select";
+import Input from "./Input";
+export {
+    Header,
+    Button,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    SignUp,
+    Login,
+    PostCard,
+    PostForm,
+    Input,
+    AuthLayout,
+    Select
+};
